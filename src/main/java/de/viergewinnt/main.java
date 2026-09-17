8cc96c5bb04d3547b7b766d0951fc333074b8393
Schlapperklange <schmaluten73@gmail.com>
2026-09-17 10:23:48 +0200

    Fix malformed Maven POM and Java System reference

--- src/main/java/de/viergewinnt/main.java
+++ src/main/java/de/viergewinnt/main.java
@@ -2,7 +2,7 @@ package de.viergewinnt;
 public class main {
 
     public static void main(String[] args) {
-        system.out.println("4 Gewinnt - Pathtraced Edition");
+        System.out.println("4 Gewinnt - Pathtraced Edition");
     }
 
 }
